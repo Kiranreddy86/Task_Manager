c45e7cdf63ad9cdc650c5a0f593ee30d7241a4ca
kiran reddy <chandrakiranreddyramana7780@gmail.com>
2024-02-25 18:47:11 +0530

    Initial Setup

---
+++ src/test/java/com/Project/Task_Manager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.Task_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
